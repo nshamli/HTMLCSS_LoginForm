31bacdc25dd663d4a9f6eb4bceae64a6261636de
nshamli <nshamli45@gmail.com>
2022-09-29 00:30:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const username =document.getElementById('name');
+const password =document.getElementById('password');
+const button =document.getElementById('connect');
+const spinner =document.getElementById('spinner');
+const check =document.getElementById('check');
+
+var username_valid=false;
+var password_valid=false;
+
+username.addEventListener('input',function(){
+    if(this.value.length > 4){
+        username_valid=true;
+    }
+    else{
+        username_valid=false;
+    }
+    form_valid(username_valid,password_valid);
+}) 
+
+
+
+password.oninput = function(){
+    if(this.value.length > 8){
+        password_valid=true;
+    }
+    else{
+        password_valid=false;
+    }
+    form_valid(username_valid,password_valid);
+}
+
+
+function form_valid(username_valid,password_valid){
+    if(username_valid && password_valid){
+        spinner.style.display='none';
+        check.style.display='block';
+        button.classList.add('valid');
+
+    }
+    else{
+        spinner.style.display='block';
+        check.style.display='none';
+        button.classList.remove('valid');
+    }
+}
